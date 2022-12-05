067a8e4a891f5372890b5181ee3c07bea8da6e56
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-12-05 16:42:34 -0500

    update vehicle route for maintenance logs

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
-import { addVehicle, getAllVehicles } from "./vehicles.js";
+import { addVehicle, getAllVehicles, updateLogs } from "./vehicles.js";
 
 const app = express();
 app.use(express.json({limit:"5mb"}));
 app.use(cors());
 
-app.listen(process.env.PORT, () => console.log(`API running on port ${process.env.PORT}`))
-
 app.post("/", addVehicle)
 app.get("/", getAllVehicles)
+app.patch("/:vehicle_id", updateLogs)
+
+app.listen(process.env.PORT, () => console.log(`API running on port ${process.env.PORT}`))
--- vehicles.js
+++ vehicles.js
@@ -1,4 +1,5 @@
 import { vehicles } from "./mongoConnect.js";
+import { ObjectId } from "mongodb";
 
 export const addVehicle = async (req, res) => {
   console.log(req.body)
@@ -9,4 +10,17 @@ export const addVehicle = async (req, res) => {
 export const getAllVehicles =  async (req, res) => {
   const allVehicles = await vehicles.find().toArray()
   res.send(allVehicles)
+};
+
+export const updateLogs = async (req, res) => {
+  const {vehicle_id} = req.params
+  
+  await vehicles
+    .findOneAndUpdate({ _id: new ObjectId(vehicle_id)}, { $set: req.body})
+    .catch(err => {
+      res.status(500).send(err)
+      return
+    })
+  res.send(req.body)
+  //sending back body of updated vehicle
 };
